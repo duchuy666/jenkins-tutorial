c5d17cc7d6dbde750046bd2c2db77b6540f521ad
Shyn <92500533+duchuy666@users.noreply.github.com>
2026-07-08 09:36:17 +0700

    Add unit tests for add and is_even functions

---
+++ test_app.py
@@ -0,0 +1,9 @@
+from app import add, is_even
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+def test_is_even():
+    assert is_even(4) == True
+    assert is_even(7) == False
